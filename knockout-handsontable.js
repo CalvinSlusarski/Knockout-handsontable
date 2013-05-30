35498c0375735826e5574d13641fabe179ad68ce
papablopo <papablopo@gmail.com>
2013-05-30 08:41:35 -0500

    colums type property fixed
    colums type property fixed

--- knockout-handsontable.js
+++ knockout-handsontable.js
@@ -10,23 +10,6 @@
     var unwrap = ko.utils.unwrapObservable;
 
     var peekByString = function (o, s) {
-        if(s)
-    		{
-		        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-		        s = s.replace(/^\./, '');           // strip a leading dot
-		        var a = s.split('.');
-		        while (a.length) {
-		            var n = a.shift();
-		            if (n in o) {
-		                o = o[n];
-		            } else {
-		                return;
-		            }
-		        }
-		        return o;
-    		}
-    };
-    var peekByString = function (o, s) {
     	if(s)
     		{
 		        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
